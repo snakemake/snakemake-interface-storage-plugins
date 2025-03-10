7361b535198a941a9ef3bd4357976f84011476de
Matthew Monk <monk.matthew@gmail.com>
2025-03-10 13:31:28 +0100

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- snakemake_interface_storage_plugins/storage_object.py
+++ snakemake_interface_storage_plugins/storage_object.py
@@ -144,21 +144,23 @@ class StorageObjectRead(StorageObjectBase):
             async with self._rate_limiter(Operation.SIZE):
                 return self.size()
         except Exception as e:
-            raise WorkflowError(f"Failed to get size of {self.print_query}", e)
+            raise WorkflowError(f"Failed to get size of {self.print_query}") from e
 
     async def managed_mtime(self) -> float:
         try:
             async with self._rate_limiter(Operation.MTIME):
                 return self.mtime()
         except Exception as e:
-            raise WorkflowError(f"Failed to get mtime of {self.print_query}", e)
+            raise WorkflowError(f"Failed to get mtime of {self.print_query}") from e
 
     async def managed_exists(self) -> bool:
         try:
             async with self._rate_limiter(Operation.EXISTS):
                 return self.exists()
         except Exception as e:
-            raise WorkflowError(f"Failed to check existence of {self.print_query}", e)
+            raise WorkflowError(
+                f"Failed to check existence of {self.print_query}"
+            ) from e
 
     async def managed_retrieve(self):
         try:
@@ -174,8 +176,8 @@ class StorageObjectRead(StorageObjectBase):
                 else:
                     os.remove(local_path)
             raise WorkflowError(
-                f"Failed to retrieve storage object from {self.print_query}", e
-            )
+                f"Failed to retrieve storage object from {self.print_query}"
+            ) from e
 
 
 class StorageObjectWrite(StorageObjectBase):
@@ -224,4 +226,6 @@ class StorageObjectTouch(StorageObjectBase):
             async with self._rate_limiter(Operation.TOUCH):
                 self.touch()
         except Exception as e:
-            raise WorkflowError(f"Failed to touch storage object {self.print_query}", e)
+            raise WorkflowError(
+                f"Failed to touch storage object {self.print_query}"
+            ) from e
