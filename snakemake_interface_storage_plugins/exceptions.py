a83ce5a2b6d7671731aa3a83fd5ad0379f28b0de
Johannes Koester <johannes.koester@uni-due.de>
2025-12-01 11:14:24 +0100

    fix

--- snakemake_interface_storage_plugins/exceptions.py
+++ snakemake_interface_storage_plugins/exceptions.py
@@ -1,10 +1,14 @@
 from pathlib import Path
+from typing import Optional
 
 
 class FileOrDirectoryNotFoundError(Exception):
-    def __init__(self, query: str, local_path: Path):
-        self.query: str = query
+    def __init__(self, local_path: Path, query: Optional[str]):
+        self.query: Optional[str] = query
         self.local_path: Path = local_path
-        super().__init__(
-            f"Storage object {query} not found in storage (local path: {local_path})."
+        msg = (
+            f"Storage object {query} not found in storage (local path: {local_path!s})."
+            if query
+            else f"File or directory not found: {local_path!s}"
         )
+        super().__init__(msg)
